255a506fff8021e3add4d7cf3a0a17864a40e150
henyxia <henyxia@live.fr>
2015-04-08 12:09:06 +0200

    Thread now totally working Need to add detached state

--- Serveur/serveur.c
+++ Serveur/serveur.c
@@ -1,20 +1,40 @@
-/** Fichier serveur.c **/
+#include <stdio.h>
+#include <pthread.h>
+#include <libthrd.h>
+#include "tcp.h"
+#include "udp.h"
 
-/***********************************************************/
-/** Serveur pour le serveur                               **/
-/***********************************************************/
-
-/** Fichiers d'inclusion **/
-
-/** Constantes **/
-
-/** Variables publiques **/
-
-/** Variables statiques **/
-
-/** Procedure principale **/
-
-int main(int argc,char **argv)
+void heellllo(void* arg)
 {
-	return(0);
+	printf("Helllloooo ! \n");
+}
+
+int main(int argc,char *argv[])
+{
+	//int ret;
+	newThread(heellllo, NULL, 0);
+/*
+	int sUDP = initUDPServer();
+	if(sUDP != SOCKET_ERROR)
+	{
+		//ret = newThread(processUDPServer, &sUDP, sizeof(sUDP));
+		if(ret != 0)
+			return 32 + ret;
+	}
+	else
+		return 1;
+
+	int sTCP = initTCPServer();
+	if(sTCP != SOCKET_ERROR)
+	{
+		ret = pthread_create(&tTCP, NULL, processTCPServer, (void*) &sTCP);
+		if(ret != 0)
+		{
+			printf("TCP Thread failed starting\n");
+			return 3;
+		}
+	}
+*/
+	while(1);
+	return 0;
 }
--- Threads/libthrd.c
+++ Threads/libthrd.c
@@ -1,8 +1,44 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <pthread.h>
 #include "libthrd.h"
 
-int newThread(void(*func) (void*), void* param)
+#define MAX_THREAD	100
+
+typedef struct
 {
+	void (*func) (void*);
+	void* param;
+}rFunc, *pFunc;
+
+int threadStarted = 0;
+pthread_t thread[MAX_THREAD];
+
+void* processThread(void* arg)
+{
+	pFunc tArg = arg;
+	tArg->func(tArg->param);
+	free(tArg->param);
+	free(tArg);
+	return NULL;
+}
+
+int newThread(void(*func) (void*), void* param, int size)
+{
+	pFunc newParam = (pFunc) malloc(sizeof(rFunc));
+	if(newParam == NULL)
+		return 1;
+	newParam->func = func;
+	newParam->param = (pFunc) malloc(size);
+	if(newParam->param == NULL)
+		return 2;
+	if(size > 0)
+		memcpy(newParam->param, param, size);
+	//TODO
+	//Add detached state
+	if(pthread_create(&(thread[threadStarted]), NULL, processThread, newParam) != 0)
+		return 3;
 
 	return 0;
 }
