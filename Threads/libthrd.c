e7a1ee171b749f0c41180cc74a612eed6b317981
henyxia <henyxia@live.fr>
2015-04-24 19:16:28 +0200

    Added IPC semaphores

--- Threads/libthrd.c
+++ Threads/libthrd.c
@@ -3,9 +3,13 @@
 #include <string.h>
 #include <pthread.h>
 #include <unistd.h>
+#include <sys/types.h>
+#include <sys/ipc.h>
+#include <sys/sem.h>
 #include "libthrd.h"
 
 #define MAX_THREAD	100
+#define KEY			0x1100
 
 typedef struct
 {
@@ -13,8 +17,9 @@ typedef struct
 	void* param;
 }rFunc, *pFunc;
 
-int threadStarted = 0;
-pthread_t thread[MAX_THREAD];
+int			threadStarted = 0;
+pthread_t	thread[MAX_THREAD];
+int			mySem;
 
 void* processThread(void* arg)
 {
@@ -58,10 +63,76 @@ void waitForThreads()
 	}
 }
 
-void P(int team)
+int get_sem(int i)
 {
+	ushort semarr[30];
+	union semun
+	{
+		int val;
+		struct semid_ds *buf;
+		ushort *array;
+	}arg;
+
+	arg.array = semarr;
+	semctl(mySem, i, GETALL, arg);
+	return semarr[i];
 }
 
-void V(int team)
+void show_sem(int i)
 {
+	int val;
+	val = get_sem(i);
+	printf("semaphore[%d]=%d\n", i, val);
+}
+
+void create_sem(int N)
+{
+#ifdef DEBUG
+	printf("Creating %d semaphores\n", N);
+#endif
+	mySem = semget(KEY, N, 0666 | IPC_CREAT);
+	if(mySem < 0)
+		printf("Unable to create the semaphore\n");
+}
+
+void init_sem(int N)
+{
+	int j;
+	int retval;
+	union semun
+	{
+		int val;
+		struct semid_ds *buf;
+		ushort *array;
+	}arg;
+	arg.val = 1;
+	for (j=0; j<N; j++)
+	{
+		retval = semctl(mySem, j, SETVAL, arg);
+		if(retval < 0)
+			printf("Unable to initialize the semaphore\n");
+	}
+}
+
+void PV(int i, int act)
+{
+	struct sembuf op;
+	int retval;
+
+	op.sem_num = i;
+	op.sem_op = act; //1=V, -1=P
+	op.sem_flg = 0; //will wait
+	retval = semop(mySem, &op, 1);
+	if(retval != 0)
+		printf("Error semop will do %d\n", act);
+}
+
+void P(int i)
+{
+	PV(i, -1);
+}
+
+void V(int i)
+{
+	PV(i, 1);
 }
