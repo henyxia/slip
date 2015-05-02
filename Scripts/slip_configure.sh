f099903bdec72b476a11b2cb59ad6405eceef293
henyxia <henyxia@live.fr>
2015-05-02 18:03:04 +0200

    Removed useless local-mounted line

--- Scripts/slip_configure.sh
+++ Scripts/slip_configure.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
-
 slattach -L -s 9600 -p slip /dev/ttyUSB0 &
 sleep 1
-#ifconfig sl0 192.168.0.1 dstaddr 192.168.1.4 mtu 1500
 ip link set sl0 up
 ip address add dev sl0 172.26.79.200 peer 172.26.79.201
 echo 1 > /proc/sys/net/ipv4/ip_forward
