34407e0ecc042255f698adf7ecf042520789384a
henyxia <henyxia@live.fr>
2015-05-04 10:07:31 +0200

    Added option parser

--- Serveur/serveur.c
+++ Serveur/serveur.c
@@ -5,9 +5,13 @@
 #include <stdbool.h>
 #include <signal.h>
 #include <libcom.h>
+#include <getopt.h>
+#include <string.h>
 #include "http.h"
 #include "teams.h"
 #include "udp.h"
+
+#define	MAX_PORT	5
 
 struct sigaction	action;
 bool				stop;
@@ -34,6 +38,50 @@ void startUDPServer(void* arg)
 
 int main(int argc,char *argv[])
 {
+	// Option parser
+	char			ret;
+	const char*		short_opt = "hp:";
+	char			port[MAX_PORT];
+	strcpy(port, "80");
+	struct option	long_opt[] =
+	{
+		{"help",	no_argument,		NULL,	'h'},
+		{"port",	required_argument,	NULL,	'p'},
+		{NULL,		0,					NULL,	0}
+	};
+
+	while((ret = getopt_long(argc, argv, short_opt, long_opt, NULL)) != -1)
+	{
+		switch(ret)
+		{
+			case -1:       /* no more arguments */
+			case 0:        /* long options toggles */
+				break;
+
+			case 'p':
+				sprintf(port, "%s", optarg);
+				printf("Overriding default port (80) with port %s\n", port);
+				break;
+
+			case 'h':
+				printf("Usage: %s [OPTIONS]\n", argv[0]);
+				printf("\t-p, --port <PORT>\t\tOverride default port (80) with <PORT>\n");
+				printf("\t-h, --help\t\tPrint this help and exit\n");
+				printf("\n");
+				return(0);
+
+			case ':':
+			case '?':
+				fprintf(stderr, "Try `%s --help' for more information.\n", argv[0]);
+				return(-2);
+
+			default:
+				fprintf(stderr, "%s: invalid option -- %c\n", argv[0], ret);
+				fprintf(stderr, "Try `%s --help' for more information.\n", argv[0]);
+				return(-2);
+      };
+   };
+
 	// Yep, we want to run the soft
 	stop = false;
 
@@ -52,7 +100,7 @@ int main(int argc,char *argv[])
 		printf("UDP Init failed\n");
 
 	// TCP
-	int sTCP = initialisationServeur("8080");
+	int sTCP = initialisationServeur(port);
 	if(sTCP != SOCKET_ERROR)
 		newThread(startTCPServer, &sTCP, sizeof(sTCP));
 	else
